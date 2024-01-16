274d44bb6b860f8140504777c39b8a00f95536b0
L_OSORIO <luis.osorio@telefonica.com>
2024-01-16 07:36:23 -0300

    cambios

--- App.tsx
+++ App.tsx
@@ -1,83 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './src/navigation/Navigation';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'white';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          
-          <Text>
-          Este es el nuevo Comentario AUTOMATICO...
-        
-        </Text> 
-
-        </View>
-
-
-
-
-      </ScrollView>
-    </SafeAreaView>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Bastará con ejecutar este comando:
 
 ```npm run android```
 
-** Links externos ** 
+**Links externos** 
 Esta es el backend que consume esta aplicacion. Usuario: losorio75
 
 https://www.themoviedb.org/?language=es
@@ -20,4 +20,39 @@ https://www.themoviedb.org/?language=es
 Para acceder tenes que tener un usuario y password ya que son personalizados.
 Para ver mi token tenes que iniciar, ir a Configuraciones y luego a API. Ahi vas a tener la clave API y el Token de acceso.
 
+**Fase de Navegacion**
+Para la navegación tendremos que hacer lo siguiente:
+
+sitio oficial para copiarnos el navegador
+https://reactnavigation.org/docs/getting-started
+
+***Paso 1***
+```npm install @react-navigation/native```
+
+***Paso 2***
+Tenemos que instalar estas dependencias
+
+```npm install react-native-screens react-native-safe-area-context```
+
+***Paso 3***
+En este paso ya podemos utilizar por medio del import la navegación en nuestro archivo principal de app.tsx.
+
+```
+import { NavigationContainer } from '@react-navigation/native';
+```
+
+***Paso 4***
+Ahora falta instalar la parte de stack (eso esta en Navitators/Stack)
+
+```npm install @react-navigation/stack```
+
+***Paso 5***
+Creamos el archivo navigation.tsx que contiene todas las paginas que voy a navegar.
+
+
+
+**Notas**
+Si vamos a trabajar en IOS hay que eliminar el uso de flipper
+
+```ios/PodFileuse_flipper!()```
 
